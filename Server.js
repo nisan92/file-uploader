3548fe07efee884ec0defd67a0a2c9910e5067ad
MKhossain <mustafa@cansoft.com>
2017-11-16 18:33:03 +0600

    File uploader using multer node js package

---
+++ Server.js
@@ -0,0 +1,36 @@
+var express =   require("express");
+var bodyParser =    require("body-parser");
+var multer  =   require('multer');
+var app =   express();
+app.use(bodyParser.json());
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, '/uploads/media');
+  },
+  filename: function (req, file, callback) {
+    // console.log(req.files);
+    callback(null, file.fieldname + '-' + Date.now() + file.originalname);
+  }
+});
+var upload = multer({ storage : storage }).array('sampleFile',5);
+
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/api/photo',function(req,res){
+        // console.log(req);
+        // console.log(res);
+    upload(req,res,function(err) {
+        // console.log(req.body);
+        console.log(req.files);
+        if(err) {
+            return res.end("Error uploading file." + err);
+        }
+        res.end("File is uploaded");
+    });
+});
+
+app.listen(3000,function(){
+    console.log("Working on port 3000");
+});
